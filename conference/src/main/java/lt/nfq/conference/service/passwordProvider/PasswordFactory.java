28cbdaf132fcdcada8ceaf640087c4804a2fd2b9
EgidijusSadauskas <sadauskas.egidijus@gmail.com>
2013-12-09 17:12:07 +0200

    Final fixes

--- conference/src/main/java/lt/nfq/conference/service/passwordProvider/PasswordFactory.java
+++ conference/src/main/java/lt/nfq/conference/service/passwordProvider/PasswordFactory.java
@@ -8,10 +8,17 @@ public class PasswordFactory {
 	
 	public static String getEncodedPassword(String password){
 		String encodedPassword;
+		String salt = "Egidijus";
 		try {
 			MessageDigest mdigest = MessageDigest.getInstance("MD5");
 			mdigest.update(password.getBytes(),0,password.length());
 			encodedPassword = new BigInteger(1,mdigest.digest()).toString(16);
+			
+			encodedPassword = encodedPassword + salt;
+			mdigest.update(encodedPassword.getBytes(), 0, encodedPassword.length());
+			encodedPassword = new BigInteger(1, mdigest.digest()).toString();
+			
+			
 		} catch (NoSuchAlgorithmException e) {
 			encodedPassword = "error";
 			e.printStackTrace();
--- conference/src/main/resources/sql/sample-data.sql
+++ conference/src/main/resources/sql/sample-data.sql
@@ -8,9 +8,10 @@ INSERT INTO conferences (conferenceId, conferenceName,location ,description, sta
 INSERT INTO conferences (conferenceId, conferenceName,location ,description, startDate, endDate,ownerId) VALUES (2, 'Projekto valdymas ir Agile','lietuva','neblogas bus atidarymas', '2013-10-02 17:00:00', '2013-10-02 18:00:00',1);
 INSERT INTO conferences (conferenceId, conferenceName,location ,description, startDate, endDate,ownerId) VALUES (3, 'J2EE programavimui naudojami įrankiai','lietuva','neblogas bus atidarymas', '2013-10-07 17:00:00', '2013-10-07 18:00:00',1);
 INSERT INTO conferences (conferenceId, conferenceName,location ,description, startDate, endDate,ownerId) VALUES (4, 'Web puslapių vartotojo sąsajos kūrimas (1-a dalis)','lietuva','neblogas bus atidarymas', '2013-10-09 17:00:00', '2013-10-09 18:00:00',1);
-INSERT INTO conferences (conferenceId, conferenceName,location ,description, startDate, endDate,ownerId) VALUES (5, 'Raketos sumontavimas iš produktų esančių jūsų namo rūsyje','lietuva','Skrisim', '2013-10-08 17:00:00', '2013-10-09 19:00:00',1);
-
-
+INSERT INTO conferences (conferenceId, conferenceName,location ,description, startDate, endDate,ownerId) VALUES (5, 'Raketos sumontavimas iš produktų esančių jūsų namo rūsyje2','lietuva','Skrisim', '2013-10-08 17:00:00', '2013-10-09 19:00:00',1);
+INSERT INTO conferences (conferenceId, conferenceName,location ,description, startDate, endDate,ownerId) VALUES (6, 'Raketos sumontavimas iš produktų esančių jūsų namo rūsyje3','lietuva','Skrisim', '2013-10-08 17:00:00', '2013-10-09 19:00:00',1);
+INSERT INTO conferences (conferenceId, conferenceName,location ,description, startDate, endDate,ownerId) VALUES (7, 'Raketos sumontavimas iš produktų esančių jūsų namo rūsyje4','lietuva','Skrisim', '2013-10-08 17:00:00', '2013-10-09 19:00:00',1);
+INSERT INTO conferences (conferenceId, conferenceName,location ,description, startDate, endDate,ownerId) VALUES (8, 'Raketos sumontavimas iš produktų esančių jūsų namo rūsyje5','lietuva','Skrisim', '2013-10-08 17:00:00', '2013-10-09 19:00:00',1);
 INSERT INTO categories (categoryId,categoryName) VALUES (1,'Programming');
 INSERT INTO categories (categoryId,categoryName) VALUES (2,'Rocket science');
 INSERT INTO categories (categoryId,categoryName) VALUES (3,'Technical engineering');
@@ -22,6 +23,9 @@ INSERT INTO conference_categories(conferenceId,categoryId) VALUES (2,3);
 INSERT INTO conference_categories(conferenceId,categoryId) VALUES (3,2);
 INSERT INTO conference_categories(conferenceId,categoryId) VALUES (4,1);
 INSERT INTO conference_categories(conferenceId,categoryId) VALUES (5,5);
+INSERT INTO conference_categories(conferenceId,categoryId) VALUES (6,5);
+INSERT INTO conference_categories(conferenceId,categoryId) VALUES (7,5);
+INSERT INTO conference_categories(conferenceId,categoryId) VALUES (8,5);
 
 INSERT INTO conference_attendees(conferenceId,userId) VALUES(4,1);
 INSERT INTO conference_attendees(conferenceId,userId) VALUES(5,1);
@@ -34,11 +38,11 @@ INSERT INTO conference_attendees(conferenceId,userId) VALUES(1,8);
 INSERT INTO cities(cityId,cityName,countryName) VALUES (1,'afganistanas1','AF');
 INSERT INTO cities(cityId,cityName,countryName) VALUES (2,'afganistanas2','AF');
 INSERT INTO cities(cityId,cityName,countryName) VALUES (3,'afganistanas3','AF');
-INSERT INTO cities(cityId,cityName,countryName) VALUES (4,'kaunas','LT');
-INSERT INTO cities(cityId,cityName,countryName) VALUES (5,'vilnius','LT');
+INSERT INTO cities(cityId,cityName,countryName) VALUES (4,'afganistanas4','AF');
+INSERT INTO cities(cityId,cityName,countryName) VALUES (5,'kaunas','LT');
+INSERT INTO cities(cityId,cityName,countryName) VALUES (6,'vilnius','LT');
+INSERT INTO cities(cityId,cityName,countryName) VALUES (7,'vilnius','LT');
 
-INSERT INTO conferences VALUES(7,'pirmojikonferencija','ls','pirmoji legit sukurta','2013-12-08 00:00:00.000000','2013-12-18 00:00:00.000000',8);
-INSERT INTO members VALUES(8,'egidijus1','ramzzvi@gmail.com','kaunas','LS','rockas','1e6efed8a8e02519be6822a7851bb71d');
-INSERT INTO conference_categories VALUES (7,2);
-INSERT INTO MEMBERS VALUES(13,'omgazauras','ramzzvi@gmail.com','kaunas','LS','aaaaaaaaaa','e09c80c42fda55f9d992e59ca6b3307d');
+
+=
 COMMIT;
